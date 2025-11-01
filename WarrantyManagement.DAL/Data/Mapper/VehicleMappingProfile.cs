1b04741f5cd60a4f733d9d954dc09d6523d458b7
ThinhNguyenrt <171249327+ThinhNguyenrt@users.noreply.github.com>
2025-11-01 08:52:36 +0700

    get customervehicle methods

---
+++ WarrantyManagement.DAL/Data/Mapper/VehicleMappingProfile.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WarrantyManagement.DAL.Data.Entities;
+using WarrantyManagement.DAL.Data.Response;
+
+namespace WarrantyManagement.DAL.Data.Mapper
+{
+    public class VehicleMappingProfile : Profile
+    {
+        public VehicleMappingProfile()
+        {
+            CreateMap<CustomerVehicle, VehicleResponse>()
+               // Map các trường từ entity CustomerVehicle
+               .ForMember(dest => dest.VIN, opt => opt.MapFrom(src => src.VIN))
+               .ForMember(dest => dest.VehicleName, opt => opt.MapFrom(src => src.VehicleName))
+               .ForMember(dest => dest.Model, opt => opt.MapFrom(src => src.Model))
+               .ForMember(dest => dest.PurchaseDate, opt => opt.MapFrom(src => src.PurchaseDate))
+               .ForMember(dest => dest.MileAge, opt => opt.MapFrom(src => src.MileAge))
+               // Map các trường từ entity Customer
+               .ForMember(dest => dest.CustomerId, opt => opt.MapFrom(src => src.Customer.CustomerId))
+               .ForMember(dest => dest.FirstName, opt => opt.MapFrom(src => src.Customer.FirstName))
+               .ForMember(dest => dest.LastName, opt => opt.MapFrom(src => src.Customer.LastName))
+               .ForMember(dest => dest.PhoneNumber, opt => opt.MapFrom(src => src.Customer.PhoneNumber))
+               .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Customer.Email))
+               // Ignore các quan hệ không cần thiết (Campaign, VehicleParts)
+               .ForAllOtherMembers(opt => opt.Ignore());
+        }
+    }
+}
---
+++ WarrantyManagement.DAL/Data/Response/VehicleResponse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarrantyManagement.DAL.Data.Response
+{
+    public class VehicleResponse
+    {
+        // Thông tin CustomerVehicle
+        public string VIN { get; set; }
+        public string VehicleName { get; set; }
+        public string Model { get; set; }
+        public DateTime PurchaseDate { get; set; }
+        public int MileAge { get; set; }
+
+        // Thông tin Customer
+        public Guid CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+        // (Tùy chọn) Thuộc tính gộp tên đầy đủ cho tiện hiển thị
+        public string FullName => $"{FirstName} {LastName}";
+    }
+}

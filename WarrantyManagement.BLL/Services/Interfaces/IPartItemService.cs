1f2b1903a8fc15e0d8678a0e84fc913e2ac2ac67
ThinhNguyenrt <171249327+ThinhNguyenrt@users.noreply.github.com>
2025-11-02 11:08:05 +0700

    fix update claim method

--- WarrantyManagement.BLL/Services/Interfaces/IPartItemService.cs
+++ WarrantyManagement.BLL/Services/Interfaces/IPartItemService.cs
@@ -60,6 +60,6 @@ namespace WarrantyManagement.BLL.Services.Interfaces
         /// Kiểm tra PartNumber đã tồn tại chưa
         /// </summary>
         Task<bool> PartNumberExistsAsync(string partNumber, Guid? excludePartItemId = null);
-        Task HandleClaimPartItemsAsync(ClaimRequest request, WarrantyClaim claim);
+        Task HandleClaimPartItemsAsync(ClaimRequest request, WarrantyClaim claim, bool isUpdate);
     }
 }

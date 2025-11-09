e06b711b8b02f9ac78b71c3279b273029dd3e7af
ThinhNguyenrt <171249327+ThinhNguyenrt@users.noreply.github.com>
2025-11-09 11:37:31 +0700

    test policy for part items

---
+++ WarrantyManagement.DAL/Data/Response/WarrantyPolicyDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarrantyManagement.DAL.Data.Response
+{
+    public class WarrantyPolicyDto
+    {
+        public Guid PolicyId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int DurationMonth { get; set; }
+        public string Condition { get; set; }
+        public DateTime CreateTime { get; set; }
+    }
+}

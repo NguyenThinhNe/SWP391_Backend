5165788f58e6de338af094c735f2e0e021bdfc8e
ThinhNguyenrt <171249327+ThinhNguyenrt@users.noreply.github.com>
2025-11-13 22:33:36 +0700

    fix(campaign): add technican name in campaign response

--- WarrantyManagement.BLL/obj/Debug/net8.0/WarrantyManagement.BLL.AssemblyInfo.cs
+++ WarrantyManagement.BLL/obj/Debug/net8.0/WarrantyManagement.BLL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WarrantyManagement.BLL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+157326f867129b66586081cfe19ef257be937f3a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+443eb4f4b27a943bbce1962d0b128c550cba1491")]
 [assembly: System.Reflection.AssemblyProductAttribute("WarrantyManagement.BLL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WarrantyManagement.BLL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
